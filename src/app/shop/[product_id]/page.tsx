d4c175ee69684ed102b71c9b60e9c6789b97a04b
nadeem <nadeemsangrasi903@gmail.com>
2024-12-09 00:15:53 +0500

    successfully build

--- src/app/shop/[product_id]/page.tsx
+++ src/app/shop/[product_id]/page.tsx
@@ -4,6 +4,8 @@ import Wrapper from "@/components/shared/Wrapper";
 import ProductDetails from "./ProductDetail";
 import RelatedProduct from "./RelatedProduct";
 
+export const generateStaticParams = () =>
+  products.map((product) => ({ product_id: product.id.toString() }));
 const ProductPage = ({ params }: { params: { product_id: string } }) => {
   const { product_id } = params;
   const product = products.find((product) => product.id === product_id)!;

99b3e50ab238325b6ae6d3d71239e8ebcbd7f198
nadeem <nadeemsangrasi903@gmail.com>
2025-01-20 10:50:11 +0500

    change

--- src/components/shared/WishList.tsx
+++ src/components/shared/WishList.tsx
@@ -78,7 +78,7 @@ function WishList() {
           </div>
         </div>
         <button
-          onClick={toggleCart}
+          onClick={toggleWishList}
           className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
           aria-label="Close Cart"
         >

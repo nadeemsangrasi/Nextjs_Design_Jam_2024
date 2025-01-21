4677ded745c6afea8ae917e2049d05e0a190c9a7
nadeem <nadeemsangrasi903@gmail.com>
2025-01-22 04:10:25 +0500

    ready to deploy

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,9 +1,9 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   apiVersion: "2021-03-25",
-  useCdn: false,
-  token: process.env.SANITY_API_TOKEN!,
+  token: process.env.SANITY_API_TOKEN,
+  useCdn: process.env.NODE_ENV === "production",
 });
